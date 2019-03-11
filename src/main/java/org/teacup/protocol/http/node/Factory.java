c74f406d5d44dd19caf622b8e23192f748cad362
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-03-11 14:48:16 -0600

    Move all node factory methods into Factory
    To make it easier to create builders, they should be into one
    class rather than spread out into several different classes.

---
+++ src/main/java/org/teacup/protocol/http/node/Factory.java
@@ -0,0 +1,97 @@
+package org.teacup.protocol.http.node;
+
+import static org.teacup.protocol.http.node.Constants.CREATE_BUILDER;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Factory.
+ *
+ * @since 1.0
+ */
+public enum Factory {
+  ;
+
+  private static final Logger LOGGER = Logger.getLogger(Factory.class.getName());
+
+  /**
+   * Creates a new body publisher builder.
+   *
+   * @return the body publisher builder
+   */
+  public static BodyPublisherBuilder createBodyPublisherBuilder() {
+    LOGGER.log(Level.FINE, String.format(CREATE_BUILDER, "body publisher"));
+    return new BodyPublisherBuilderImpl();
+  }
+
+  /**
+   * Creates a new duration builder.
+   *
+   * @return the duration builder
+   */
+  public static DurationBuilder createDurationBuilder() {
+    LOGGER.log(Level.FINE, String.format(CREATE_BUILDER, "duration"));
+    return new DurationBuilderImpl();
+  }
+  /**
+   * Creates a new principal builder.
+   *
+   * @return the principal builder
+   */
+  public static PrincipalBuilder createPrincipalBuilder() {
+    LOGGER.log(Level.FINE, String.format(CREATE_BUILDER, "principal"));
+    return new PrincipalBuilderImpl();
+  }
+
+  /**
+   * Creates a new request builder.
+   *
+   * @return the request builder
+   */
+  public static RequestBuilder createRequestBuilder() {
+    LOGGER.log(Level.FINE, String.format(CREATE_BUILDER, "request"));
+    return new RequestBuilderImpl();
+  }
+
+  /**
+   * Creates a new response builder.
+   *
+   * @param <T> the response body type
+   * @return the response builder
+   */
+  public static <T> ResponseBuilder<T> createResponseBuilder() {
+    LOGGER.log(Level.FINE, String.format(CREATE_BUILDER, "response"));
+    return new ResponseBuilderImpl<>();
+  }
+
+  /**
+   * Create a new SSL session builder.
+   *
+   * @return the SSL session builder
+   */
+  public static SslSessionBuilder createSslSessionBuilder() {
+    LOGGER.log(Level.FINE, String.format(CREATE_BUILDER, "SSL session"));
+    return new SslSessionBuilderImpl();
+  }
+
+  /**
+   * Create a new SSL session context builder.
+   *
+   * @return the SSL session context builder
+   */
+  public static SslSessionContextBuilder createSslSessionContextBuilder() {
+    LOGGER.log(Level.FINE, String.format(CREATE_BUILDER, "SSL session context"));
+    return new SslSessionContextBuilderImpl();
+  }
+
+  /**
+   * Create a new URI builder.
+   *
+   * @return the URI builder
+   */
+  public static UriBuilder createUriBuilder() {
+    LOGGER.log(Level.FINE, String.format(CREATE_BUILDER, "URI"));
+    return new UriBuilderImpl();
+  }
+}
