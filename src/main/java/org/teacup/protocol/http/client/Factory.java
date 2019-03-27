45187ea0f330b09b20259b452e76df47de96bc88
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-03-27 16:53:44 -0600

    Delete HandlerBuilderFactory
    The factory method is moved into the Factory class.
    Tests are moved as well.

--- src/main/java/org/teacup/protocol/http/client/Factory.java
+++ src/main/java/org/teacup/protocol/http/client/Factory.java
@@ -1,6 +1,7 @@
 package org.teacup.protocol.http.client;
 
 import java.net.http.HttpClient;
+import java.net.http.HttpResponse.BodyHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,12 +16,24 @@ public enum Factory {
   private static final Logger LOGGER = Logger.getLogger(Factory.class.getName());
 
   /**
+   * Create a new handler builder.
+   *
+   * @param bodyHandler the body handler
+   * @param <T> the response body type
+   * @return the handler builder
+   */
+  public static <T> HandlerBuilder<T> createHandlerBuilder(BodyHandler<T> bodyHandler) {
+    LOGGER.log(Level.FINE, "Creating a new handler builder");
+    return new HandlerBuilderImpl<>(bodyHandler);
+  }
+
+  /**
    * Creates a new {@link Simple}.
    *
    * @param httpClient the HTTP client
    * @return the simple
    */
-  public static Simple create(HttpClient httpClient) {
+  public static Simple createSimple(HttpClient httpClient) {
     LOGGER.log(Level.FINE, "Creating a new client");
     return new SimpleImpl(httpClient);
   }
--- src/test/java/org/teacup/protocol/http/client/FactoryTest.java
+++ src/test/java/org/teacup/protocol/http/client/FactoryTest.java
@@ -1,14 +1,16 @@
 package org.teacup.protocol.http.client;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
+import java.net.http.HttpClient;
 import java.net.http.HttpResponse.BodyHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-class HandlerBuilderFactoryTest {
+class FactoryTest {
   @Mock private BodyHandler<String> bodyHandler;
 
   @BeforeEach
@@ -17,7 +19,13 @@ class HandlerBuilderFactoryTest {
   }
 
   @Test
-  void create() {
-    assertThat(HandlerBuilderFactory.create(bodyHandler)).isInstanceOf(HandlerBuilderImpl.class);
+  void createHandlerBuilder() {
+    assertThat(Factory.createHandlerBuilder(bodyHandler)).isInstanceOf(HandlerBuilderImpl.class);
+  }
+
+  @Test
+  void createSimple() {
+    var httpClient = mock(HttpClient.class);
+    assertThat(Factory.createSimple(httpClient)).isInstanceOf(SimpleImpl.class);
   }
 }
