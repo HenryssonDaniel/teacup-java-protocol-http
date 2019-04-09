43c1793894604a5a1d82986f7c28eb379bd9adb5
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-04-09 13:54:04 -0400

    Fix constant tests
    The constant tests did not work as intended.
    Calling valueOf should do the trick instead.

--- src/test/java/io/github/henryssondaniel/teacup/protocol/http/node/ConstantsTest.java
+++ src/test/java/io/github/henryssondaniel/teacup/protocol/http/node/ConstantsTest.java
@@ -1,22 +1,12 @@
 package io.github.henryssondaniel.teacup.protocol.http.node;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
 
 class ConstantsTest {
   @Test
-  void settingThe() {
-    assertThat(Constants.SETTING_THE).isEqualTo("Setting the {0}");
-  }
-
-  @Test
-  void settingWhether() {
-    assertThat(Constants.SETTING_WHETHER).isEqualTo("Setting whether {0} or not");
-  }
-
-  @Test
-  void verify() {
-    assertThat(Constants.VERIFY).isEqualTo("Verify the {0}");
+  void valueOf() {
+    assertThatIllegalArgumentException().isThrownBy(() -> Constants.valueOf(Constants.SETTING_THE));
   }
 }
